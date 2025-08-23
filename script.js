e9baa7ba747b3fc054184501de4cf83ea9aa42ac
Georgia Twumasi <twumasigm@vcu.edu>
2025-08-23 19:04:42 -0400

    Implement script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Vars
-const WHITE_KEYS=['q','w','e','r','t','y','u'];
-const BLACK_KEYS=['2','3','5','6','7'];
+const WHITE_KEYS=['z','x','c','v','b','n','m'];
+const BLACK_KEYS=['s','d','f','g','h'];
 
 const whiteKeys = document.querySelectorAll('.key.white');
 const blackKeys = document.querySelectorAll('.key.black');
